2fac616f3911c62ca832926accd76883793a58b1
Fardeen Awais <fardeenjutt99@gmail.com>
2023-02-10 21:28:34 +0500

    Banner Image is added

--- components/banner.js
+++ components/banner.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 function Banner() {
   return (
-    <div></div>
-  )
+    <div className="relative">
+      <div className="flex items-center justify-center"> {/* We are making image normal instead of carousel 😗 */}
+        <Image 
+          src="https://links.papareact.com/gi1"
+          alt="Picture of the author"
+          width={1000}
+          height={600}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
